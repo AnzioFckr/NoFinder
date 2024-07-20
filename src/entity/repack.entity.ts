ee3b336de765e47b712760a1e496fd29cf017062
AnzioFckr <codenamecrystal@yahoo.Com>
2024-07-20 16:15:01 +0200

    same than the previous one
    (im hungry)

---
+++ src/entity/repack.entity.ts
@@ -0,0 +1,45 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from "typeorm";
+import { DownloadSource } from "./download-source.entity";
+
+@Entity("repack")
+export class Repack {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column("text", { unique: true })
+  title: string;
+
+  @Column("text", { unique: true })
+  magnet: string;
+
+  /**
+   * @deprecated
+   */
+  @Column("int", { nullable: true })
+  page: number;
+
+  @Column("text")
+  theguythatrepacked: string;
+
+  @Column("text")
+  fileSize: string;
+
+  @Column("datetime")
+  uploadDate: Date | string;
+
+  @ManyToOne(() => DownloadSource, { nullable: true, onDelete: "CASCADE" })
+  downloadSource: DownloadSource;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
